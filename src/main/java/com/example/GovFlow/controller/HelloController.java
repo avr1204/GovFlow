fc8fd043f4f056cefa9de6a98e07553201a0da9e
Siow Yong Hui <avr7581@gmail.com>
2026-04-18 09:24:46 +0800

    Fix homepage route

--- src/main/java/com/example/GovFlow/controller/HelloController.java
+++ src/main/java/com/example/GovFlow/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/api/health")
     public String home() {
         return "GovFlow is running!";
     }
